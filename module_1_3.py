83ac43201cfca1e2640c5df04af6a711d4d5ba4a
userMa1009 <kovalevamaria1009@gmail.com>
2024-10-14 00:17:52 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name="namber_of_zadaniya"
+print(name)
+name_a=12
+print(name_a)
+name="namber_of_our"
+print(name)
+name_b=1.5
+print(name_b)
+name="curse_Pyhton"
+print(name)
+print(name_b/name_a)
+
+
